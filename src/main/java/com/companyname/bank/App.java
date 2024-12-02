5ac5edd3b37183a0e2e46a2cae2ba63fff2d4561
MdSaad08 <116965380+MdSaad08@users.noreply.github.com>
2024-12-02 00:58:33 -0500

    change2

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World change1!" );
+        System.out.println( "Hello World change2!" );
     }
 }
